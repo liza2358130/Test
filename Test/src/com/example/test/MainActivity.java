ec6793e642e183dec4db3b496840b4ec69278c3c
liza.dev@gmail.com <liza.dev@gmail.com>
2015-02-23 23:22:08 +0800

    Signed-off-by: liza.dev@gmail.com <liza.dev@gmail.com>

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -12,10 +12,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       findViewId();
     }
 
 
-    @Override
+    private void findViewId() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
